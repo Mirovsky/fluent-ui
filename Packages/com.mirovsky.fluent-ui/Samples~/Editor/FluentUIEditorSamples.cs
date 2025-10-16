d9e808be64df73e7722adcb280f6876e8db12974
Jakub Mirovský <mirovsky.jakub@gmail.com>
2025-10-16 18:41:47 +0200

    Move to Unity 6000.0.60f1

---
+++ Packages/com.mirovsky.fluent-ui/Samples~/Editor/FluentUIEditorSamples.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEngine;
+
+namespace FluentUI.Samples.Editor
+{
+    public class FluentUIEditorSamples : EditorWindow
+    {
+        [MenuItem("Assets/Fluent UI/Samples Window")]
+        public static void ShowSamples()
+        {
+            GetWindow<FluentUIEditorSamples>().Show();
+        }
+
+        private void CreateGUI()
+        {
+            rootVisualElement
+                .Children(
+                    new Label("Fluent UI Samples")
+                        .StyleFontSize(24)
+                        .StyleUnityFontStyleAndWeight(FontStyle.Bold)
+                        .StyleWidth(Length.Percent(100))
+                        .StyleUnityTextAlign(TextAnchor.MiddleCenter),
+                    new TabView()
+                        .Children(
+                            new Tab("Simple Component")
+                                .Children(new SimpleComponentExample()),
+                            new Tab("List View")
+                                .Children(new ListViewExample()),
+                            new Tab("Data Bindings")
+                                .Children(new DataBindingsExample())
+                        )
+                );
+        }
+    }
+}
---
+++ Packages/com.mirovsky.fluent-ui/Samples~/Editor/ListViewExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UIElements;
+
+namespace FluentUI.Samples.Editor
+{
+    public class ListViewExample : VisualElement
+    {
+        private readonly List<string> _items = Enumerable.Range(0, 1000).Select(i => $"Item {i}").ToList();
+
+        public ListViewExample()
+        {
+            this.Name("ListView")
+                .Children(
+                    new ListView(_items)
+                        .MakeItem(() => new Label())
+                        .BindItem((element, i) => ((Label)element).Text(_items[i]))
+                        .StyleHeight(400)
+                );
+        }
+    }
+}
